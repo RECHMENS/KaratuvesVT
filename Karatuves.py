d94f2176ae6aaa23cd7bcf3949aff4fd5687404d
FelikssPecs <felikss.pecs@gmail.com>
2025-01-20 09:56:50 +0200

    Update Karatuves.py

--- Karatuves.py
+++ Karatuves.py
@@ -9,14 +9,15 @@ main = Canvas(logs, width=1200, height=900, bg="#E6E6E6")
 main.pack()
 
 #start
+#start virsraksts
+main.create_text(600,200, text = "Karatuves", font = ("MingLiU_HKSCS-ExtB", 60,"bold"))
 #bilde
-logo = PhotoImage(file='C:\\Users\\S202-8\\Documents\\26\\Capture.png') #Dzastina path S202-8 Fēlikssa path 202-04
+logo = PhotoImage(file='C:\\Users\\202-04\\Documents\\26\\Capture.png') #Dzastina path S202-8 Fēlikssa path 202-04
 main.create_image(600, 450, image=logo)
 #start poga 
-START_POGA = PhotoImage(file='C:\\Users\\S202-8\\Documents\\26\\START POGA.png')
+START_POGA = PhotoImage(file='C:\\Users\\202-04\\Documents\\26\\START POGA.png')
 main.create_image(600, 750, image=START_POGA)
-main.create_text(600,750, text = "START", font = ("MingLiU_HKSCS-ExtB", 40,))
-
+main.create_text(600,750, text = "START", font = ("MingLiU_HKSCS-ExtB", 60,"bold"))
 
 
 logs.mainloop()
