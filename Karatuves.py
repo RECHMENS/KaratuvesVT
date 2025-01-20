c8a034570cdad17fed313e38fb362ded0d7f9487
Dzastins <143899137+LavaDecks@users.noreply.github.com>
2025-01-20 09:33:58 +0200

    Update Karatuves.py

--- Karatuves.py
+++ Karatuves.py
@@ -1,24 +1,17 @@
-from tkinter import * 
-from tkinter import PhotoImage
+from tkinter import *
 
+height = 400
+width = 400
 
-vārdi = ["Orange","Apple","Banana"]
-
-
-#canva un logs
 logs = Tk()
 logs.title("test")
-main = Canvas(logs, width=1000, height=400, bg="#E6E6E6")
+main = Canvas(logs, width=1200, height=900, bg="#E6E6E6")
 main.pack()
 
-#Bildes path
-image_Karātuve = PhotoImage(file="C:\\Users\\202-04\\Documents\\26\\Capture.PNG") #Dzastina path S202-8 Fēlikssa path 202-04
+#start
+
+logo = PhotoImage(file='C:\\Users\\S202-8\\Documents\\26\\Capture.png')
+main.create_image(100, 20, image=logo)
 
 
-image_label = Label(logs, image=image_Karātuve)
-image_label.pack()
-
-
-
-# Start the Tkinter main loop
 logs.mainloop()
