fa12073d0e81ba16299c15ceeb24d5febeaabe63
FelikssPecs <felikss.pecs@gmail.com>
2025-01-20 09:21:12 +0200

    Update Karatuves.py

--- Karatuves.py
+++ Karatuves.py
@@ -1,22 +1,24 @@
-from tkinter import *
+from tkinter import * 
 from tkinter import PhotoImage
+
 
 vārdi = ["Orange","Apple","Banana"]
 
-height = 400
-width = 400
 
+#canva un logs
 logs = Tk()
 logs.title("test")
-main = Canvas(logs, width=1000, height=400, bg="#edf2ef")
+main = Canvas(logs, width=1000, height=400, bg="#E6E6E6")
 main.pack()
 
-# Make sure the path to the image is correct
-image = PhotoImage(file="C:\\Users\\S202-8\\Documents\\26\\Capture.PNG")
+#Bildes path
+image_Karātuve = PhotoImage(file="C:\\Users\\202-04\\Documents\\26\\Capture.PNG") #Dzastina path S202-8 Fēlikssa path 202-04
 
-# To prevent the image from being garbage collected, keep a reference to it
-image_label = Label(logs, image=image)
+
+image_label = Label(logs, image=image_Karātuve)
 image_label.pack()
+
+
 
 # Start the Tkinter main loop
 logs.mainloop()
