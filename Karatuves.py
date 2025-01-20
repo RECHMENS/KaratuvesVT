f90add93316137407a2f4324da11364d422e6406
Dzastins <143899137+LavaDecks@users.noreply.github.com>
2025-01-20 09:07:19 +0200

    Update Karatuves.py

--- Karatuves.py
+++ Karatuves.py
@@ -1,1 +1,20 @@
+from tkinter import *
+from tkinter import PhotoImage
 
+height = 400
+width = 400
+
+logs = Tk()
+logs.title("test")
+main = Canvas(logs, width=1000, height=400, bg="#edf2ef")
+main.pack()
+
+# Make sure the path to the image is correct
+image = PhotoImage(file="C:\\Users\\S202-8\\Documents\\26\\Capture.PNG")
+
+# To prevent the image from being garbage collected, keep a reference to it
+image_label = Label(logs, image=image)
+image_label.pack()
+
+# Start the Tkinter main loop
+logs.mainloop()
